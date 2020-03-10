d19cc024aef333dd995ac08de1416aef556026bd
dandeboer <ddeboer926@gmail.com>
2020-03-10 16:10:06 -0400

    add deck button will now remove its innards as well as its event listener and insert innerHTML

--- static/script.js
+++ static/script.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+let deckButton = document.querySelector("#deck-button")
+
+deckButton.addEventListener('click', removeInnards)
+
+function removeInnards() {
+    let buttonInnards = document.querySelectorAll("#button-innards")
+    console.log("works")
+    for (let innard of buttonInnards) {
+        innard.remove()   
+    }
+    deckButton.removeEventListener('click', removeInnards)
+    createDeck()
+    //add form to submit ajax to the database
+    //after submission add innerhtml again and eventlistener
+}
+
+function createDeck() {
+    deckButton.innerHTML = "<p>Hello</p>"
+
+}
