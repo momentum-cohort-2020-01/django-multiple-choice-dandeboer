ddb551a763e00f7f6a26fa5b3680e52fdcab3c9a
dandeboer <ddeboer926@gmail.com>
2020-03-10 13:22:54 -0400

    can now add empty decks through admin and display them in my main html page

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Deck, Card
 
 def home(request):
-    return render(request, 'flashcards/index.html')
+    decks = Deck.objects.all()
+    return render(request, 'flashcards/index.html', {'decks': decks})
