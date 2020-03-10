12305bde7935a8dfc90b9cd95ce1a7c13b85742c
dandeboer <ddeboer926@gmail.com>
2020-03-10 12:31:52 -0400

    created user-registration. still janky, but technically functional

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,6 +52,8 @@ INSTALLED_APPS = [
     # Project-specific
     'users',
 ]
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
 
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
