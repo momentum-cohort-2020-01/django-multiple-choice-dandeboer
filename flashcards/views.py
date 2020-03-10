a2ad2d888f47319f5639fae7d6c6d448481a38fd
dandeboer <ddeboer926@gmail.com>
2020-03-10 08:25:16 -0400

    made very minor css changes

--- flashcards/views.py
+++ flashcards/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'core/index.html')
+    return render(request, 'flashcards/index.html')
