92e874ebe5148b976079821850eee29016fd43d0
dandeboer <ddeboer926@gmail.com>
2020-03-11 14:49:43 -0400

    made some preliminary model changes

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from users.models import User
 
 class Deck(models.Model):
     title = models.CharField(max_length=50, unique=True)
     description = models.TextField(max_length=200, null=True, blank=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='decks')
     # foreignkey cards
     # topics
 
